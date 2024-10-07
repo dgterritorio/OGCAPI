fd6a0865ead4b615b03d0f61ee7f75f947c4c51d
doublebyte <doublebyte@hushmail.com>
2024-10-07 19:15:45 +0100

    - remove limit 10 on materialised view

--- postgis/scripts/create-views.sql
+++ postgis/scripts/create-views.sql
@@ -7,7 +7,7 @@ declare
     temprow record;
 BEGIN
 FOR temprow IN
-        SELECT distinct municipio FROM crus_31_julho2024 LIMIT 10
+        SELECT distinct municipio FROM crus_31_julho2024
     LOOP
         EXECUTE format ('CREATE MATERIALIZED VIEW IF NOT EXISTS v%s AS'
         ' select * from crus_31_julho2024 where municipio LIKE %L WITH DATA', 

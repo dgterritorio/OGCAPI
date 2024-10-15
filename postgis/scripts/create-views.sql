1088d31c642a4721dfe7e82c87800a3cd8e3a486
doublebyte <doublebyte@hushmail.com>
2024-10-15 18:02:42 +0100

    - put names of views in lower case - support special characters in table names

--- postgis/scripts/create-views.sql
+++ postgis/scripts/create-views.sql
@@ -9,9 +9,9 @@ BEGIN
 FOR temprow IN
         SELECT distinct municipio FROM crus_31_julho2024
     LOOP
-        EXECUTE format ('CREATE MATERIALIZED VIEW IF NOT EXISTS v%s AS'
+        EXECUTE format ('CREATE MATERIALIZED VIEW IF NOT EXISTS "v%s" AS'
         ' select * from crus_31_julho2024 where municipio LIKE %L WITH DATA', 
-        replace(temprow.municipio, ' ', ''), temprow.municipio);
+        lower(replace(temprow.municipio, ' ', '')), temprow.municipio);
     END LOOP;
 END
 $$ LANGUAGE plpgsql;

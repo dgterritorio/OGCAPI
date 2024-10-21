8dbec7277559c9aa49567cd0dcd22dd7420dd48a
Antonio Cerciello <antonio.cerciello@gmail.com>
2024-10-21 11:47:07 +0200

    Utility to upload data

---
+++ utils/create_views.py
@@ -0,0 +1,97 @@
+import psycopg2
+import argparse
+import yaml, sys
+from utils import sanitize_name, load_template, update_pygeoapi_config
+
+# Command-line arguments parser
+parser = argparse.ArgumentParser(description='Generate SQL views and PyGeoAPI YAML configuration for distinct values.')
+parser.add_argument('--database', default='geodb', help='Name of the PostgreSQL database')
+parser.add_argument('--user', required=True, help='PostgreSQL username')
+parser.add_argument('--password', required=True, help='PostgreSQL password')
+parser.add_argument('--host', default='localhost', help='PostgreSQL host, default is localhost')
+parser.add_argument('--port', default='5432', help='PostgreSQL port, default is 5432')
+parser.add_argument('--table', required=True, help='Table name to create views from')
+parser.add_argument('--column', required=True, help='Column name to use for grouping, default is municipio')
+parser.add_argument('--template', default='template_view.yml', help='Path to YAML template file, default is template.yml')
+parser.add_argument('--config', default='./docker.config.yml', help='Path to the pygeoapi configuration file to update')
+args = parser.parse_args()
+
+# Establish database connection
+DATABASE = args.database
+USER = args.user
+PASSWORD = args.password
+HOST = args.host
+PORT = args.port
+TABLE = args.table
+COLUMN = args.column
+
+# Load the YAML template
+yml_template = load_template(args.template)
+
+# Connect to PostgreSQL database
+try:
+    conn = psycopg2.connect(
+        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
+    cursor = conn.cursor()
+except Exception as e:
+    print("Error: Unable to connect to the database", file=sys.stderr)
+    print(e, file=sys.stderr)
+    sys.exit(1)
+
+# Query the specified table for unique values in the specified column
+try:
+    cursor.execute(f"SELECT DISTINCT \"{COLUMN}\" FROM {TABLE};")
+    distinct_values = cursor.fetchall()
+except Exception as e:
+    print(f"Error: Unable to query the table '{TABLE}'", file=sys.stderr)
+    print(e, file=sys.stderr)
+    cursor.close()
+    conn.close()
+    sys.exit(1)
+
+# List to store yml configuration entries
+yml_entries = []
+
+# Loop through unique values in the specified column and create SQL views and yml configuration entries
+for distinct_value_tuple in distinct_values:
+    distinct_value = distinct_value_tuple[0]
+    print(distinct_value)
+    sanitized_value = sanitize_name(distinct_value)
+    view_name = f"v_{TABLE}_{sanitized_value}"
+
+    # Create SQL view for each unique value in the specified column
+    try:
+        cursor.execute(
+            f"""
+            CREATE OR REPLACE VIEW {view_name} AS
+            SELECT * FROM {TABLE} WHERE \"{COLUMN}\" = %s;
+            """,
+            (distinct_value,)
+        )
+        conn.commit()
+        print(f"SQL view '{view_name}' created successfully.")
+    except Exception as e:
+        print(f"Error: Unable to create view for {COLUMN} '{distinct_value}'", file=sys.stderr)
+        print(e, file=sys.stderr)
+        conn.rollback()
+        continue
+
+    # Create yml configuration entry
+    yml_entry = yml_template.copy()
+    yml_entry_str = yaml.dump(yml_entry)
+    yml_entry_str = yml_entry_str.replace('column_original', distinct_value)
+    yml_entry_str = yml_entry_str.replace('column_clean', sanitized_value)
+    yml_entry_str = yml_entry_str.replace('view_name', view_name)
+    yml_entry = yaml.safe_load(yml_entry_str)
+
+    # Add a resource key for easy identification
+    yml_entry['resource_key'] = TABLE + '_' + sanitized_value
+    yml_entries.append(yml_entry)
+
+# Close the database connection
+cursor.close()
+conn.close()
+
+# Update the pygeoapi configuration
+update_pygeoapi_config(args.config, yml_entries)
---
+++ utils/upload_tables.py
@@ -0,0 +1,72 @@
+import geopandas as gpd
+from sqlalchemy import create_engine
+import argparse
+import sys
+import yaml
+from utils import load_template, update_pygeoapi_config
+
+# Command-line arguments parser
+parser = argparse.ArgumentParser(description='Upload a shapefile/geopackage to PostgreSQL and generate a YAML entry.')
+parser.add_argument('--database', default='geodb', help='Name of the PostgreSQL database')
+parser.add_argument('--user', required=True, help='PostgreSQL username')
+parser.add_argument('--password', required=True, help='PostgreSQL password')
+parser.add_argument('--host', default='localhost', help='PostgreSQL host, default is localhost')
+parser.add_argument('--port', default='5432', help='PostgreSQL port, default is 5432')
+parser.add_argument('--table', default='crus', help='Table name to create from the shapefile/geopackage')
+parser.add_argument('--primary_key', default='objectid', help='Column name to use as the primary key for the new table')
+parser.add_argument('--input', default='../data/CRUS+_31_julho2024.shp', help='Path to the shapefile or geopackage file to import')
+parser.add_argument('--template', default='template_table.yml', help='Path to YAML template file, default is template.yml')
+parser.add_argument('--config', default='./docker.config.yml', help='Path to the pygeoapi configuration file to update')
+args = parser.parse_args()
+
+# Establish database connection
+DATABASE = args.database
+USER = args.user
+PASSWORD = args.password
+HOST = args.host
+PORT = args.port
+TABLE = args.table
+INPUT_FILE = args.input
+PRIMARY_KEY = args.primary_key
+
+# Load the YAML template
+yml_template = load_template(args.template)
+
+# Import shapefile or geopackage to PostgreSQL
+try:
+    # Create SQLAlchemy engine for database connection
+    engine = create_engine(f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
+
+    # Load the input file using GeoPandas
+    if INPUT_FILE.endswith('.shp'):
+        gdf = gpd.read_file(INPUT_FILE)
+    elif INPUT_FILE.endswith('.gpkg'):
+        gdf = gpd.read_file(INPUT_FILE, layer=0)
+    else:
+        print("Error: Unsupported file format. Please provide a shapefile (.shp) or a geopackage (.gpkg).", file=sys.stderr)
+        sys.exit(1)
+
+    # Check projection and convert to EPSG:4326 if necessary
+    if gdf.crs is not None and gdf.crs.to_epsg() != 4326:
+        print(f"Converting CRS from {gdf.crs} to EPSG:4326.")
+        gdf = gdf.to_crs(epsg=4326)
+
+    # Write GeoDataFrame to PostgreSQL with specified primary key
+    gdf.to_postgis(TABLE, engine, if_exists='replace', index=True, index_label=PRIMARY_KEY)
+    print(f"Table '{TABLE}' created successfully in the database from the file '{INPUT_FILE}'.")
+except Exception as e:
+    print("Error: Unable to import the input file to the database", file=sys.stderr)
+    print(e, file=sys.stderr)
+    sys.exit(1)
+
+# Create a single YAML configuration entry
+yml_entry = yml_template.copy()
+yml_entry_str = yaml.dump(yml_entry)
+yml_entry_str = yml_entry_str.replace('my_table_name', TABLE.lower())
+yml_entry = yaml.safe_load(yml_entry_str)
+
+# Add a resource key for easy identification
+yml_entry['resource_key'] = TABLE.lower()
+
+# Update the pygeoapi configuration
+update_pygeoapi_config(args.config, [yml_entry])

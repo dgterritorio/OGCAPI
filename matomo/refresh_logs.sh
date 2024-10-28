ad284f53bd41c3bc9dad90ef291c5b30fe3ac93a
Antonio Cerciello <antonio.cerciello@gmail.com>
2024-10-28 14:37:39 +0100

    Fix matomo missing logs and adding environment password

--- matomo/refresh_logs.sh
+++ matomo/refresh_logs.sh
@@ -2,5 +2,5 @@
 # Reload log files into matomo
 
 python3 /var/www/html/misc/log-analytics/import_logs.py --url=http://matomo \
- --login=user --password=matomo --idsite=1 --recorders=4 /apache/access_log.log \
+ --login=$USER_MATOMO --password=$PASSWORD_MATOMO --idsite=1 --recorders=4 /apache/access_log.log \
 && /var/www/html/console core:archive --force-all-websites --url='http://matomo'

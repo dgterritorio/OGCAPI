82e88742cbe7219ed9a459a6c54db87ba40e3a97
Antonio Cerciello <antonio.cerciello@gmail.com>
2024-10-29 11:18:59 +0100

    Cronjob variable fix

---
+++ matomo/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+
+cron 
+
+tail -f /var/log/cron.log
--- matomo/refresh_logs.sh
+++ matomo/refresh_logs.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env bash
-# Reload log files into matomo
+#!/bin/bash
+
+echo "*** Start import with following username"
+echo $USER_MATOMO
 
 python3 /var/www/html/misc/log-analytics/import_logs.py --url=http://matomo \
  --login=$USER_MATOMO --password=$PASSWORD_MATOMO --idsite=1 --recorders=4 /apache/access_log.log \
 && /var/www/html/console core:archive --force-all-websites --url='http://matomo'
+
+echo "*** Finish import with credentials"

a2a6f16bd485017b47ce7a85245c41037e001324
Antonio Cerciello <antonio.cerciello@gmail.com>
2024-11-06 15:17:49 +0100

    Making log ingestion more precise

--- matomo/refresh_logs.sh
+++ matomo/refresh_logs.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 
 echo "*** Start import with following username"
-echo $USER_MATOMO
 
+INPUT_FILE="/apache/access_log.log"
+NUM_ROWS="1000"
+DESTINATION_FILE="/tmp/temp.log"
+
+# File to store last read line
+LAST_LINE_FILE=".lastline_$(basename "$INPUT_FILE")"
+
+# Check of last line already exists
+if [ -f "$LAST_LINE_FILE" ]; then
+    LAST_LINE=$(cat "$LAST_LINE_FILE")
+else
+    LAST_LINE=1
+fi
+
+# Calculate last row
+END_LINE=$((LAST_LINE + NUM_ROWS - 1))
+
+# Number of lines in INPUT file
+TOTAL_LINES=$(wc -l < "$INPUT_FILE")
+
+# Check if there are new lines to read
+if [ "$TOTAL_LINES" -lt "$LAST_LINE" ]; then
+    echo "There are no new lines to read."
+    exit 0
+fi
+
+# If END_LINE is greater of end line, select last line of the file
+if [ "$TOTAL_LINES" -lt "$END_LINE" ]; then
+    END_LINE="$TOTAL_LINES"
+fi
+
+# Extract the set to ingest
+sed -n "${LAST_LINE},${END_LINE}p" "$INPUT_FILE" > "$DESTINATION_FILE"
+
+# Update LAST_LINE for the next run
+LAST_LINE=$((END_LINE + 1))
+echo "$LAST_LINE" > "$LAST_LINE_FILE"
+
+# Ingest the set of rows of this run
 python3 /var/www/html/misc/log-analytics/import_logs.py --url=http://matomo \
- --login=$USER_MATOMO --password=$PASSWORD_MATOMO --idsite=1 --recorders=4 /apache/access_log.log \
+ --login=$USER_MATOMO --password=$PASSWORD_MATOMO --idsite=1 --recorders=4 /tmp/temp.log \
 && /var/www/html/console core:archive --force-all-websites --url='http://matomo'
 
 echo "*** Finish import with credentials"
